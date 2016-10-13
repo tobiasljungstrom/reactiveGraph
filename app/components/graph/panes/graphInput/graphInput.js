6a9e111fd67f41cb351d26e1c2d538cc97ac53d5
Tobias <tobias.ljungstrom@gmail.com>
2016-10-13 15:28:24 +0200

    Add updating columns based on inputs

--- app/components/graph/panes/graphInput/graphInput.js
+++ app/components/graph/panes/graphInput/graphInput.js
@@ -2,18 +2,26 @@ var React = require('react');
 var InputField = require('./inputField/inputField');
 
 var GraphInput = React.createClass({
+    propTypes:{
+        data: React.PropTypes.array,
+        updateData: React.PropTypes.func
+    },
+
+    updateData: this.props.updateData;
+    },
 
     render: function() {
+
+        var fields = [];
+
+        for (var i = 0; i < this.props.data.length; i++) {
+            fields[i] = <InputField key={i} inputId={i} defaultValue={this.props.data[i]} updateColumn={this.updateData}/>;
+        }
         return (
             <div className='graphInput'>
-                <InputField />
-                <InputField />
-                <InputField />
-                <InputField />
-                <InputField />
-                <InputField />
+                {fields}
             </div>
-            );
+        );
     }
 });
 
--- app/components/graph/panes/graphInput/inputField/inputField.js
+++ app/components/graph/panes/graphInput/inputField/inputField.js
@@ -1,11 +1,23 @@
 var React = require('react');
 
 var InputField = React.createClass({
+    propTypes: {
+        defaultValue: React.PropTypes.number,
+        updateColumn: React.PropTypes.func,
+        inputId: React.PropTypes.number
+    },
+
+    handleChange: function(event) {
+        this.props.updateColumn(this.props.inputId, event.target.value);
+    },
 
     render: function() {
+
+
+
         return (
             <div className='inputField'>
-                <input type="text"/>
+                <input type="text" value={this.props.defaultValue} onChange={this.handleChange}/>
             </div>
             );
     }
